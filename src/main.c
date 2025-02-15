f7f4421b3082664b99facc23d3af7f0c4aa0de3e
Abhiuday Surya <abhiudaysurya@gmail.com>
2025-02-15 18:13:13 +0530

    format main.c

--- src/main.c
+++ src/main.c
@@ -1,105 +1,67 @@
 #include <windows.h>
 
-LRESULT CALLBACK
-MainWindowCallback(HWND Window,
-		UINT Message,
-		WPARAM wParam,
-		LPARAM lParam
-		)
-{  
-	LRESULT result;
-	switch (Message)
-	{
-		case WM_SIZE:
-		{
-			OutputDebugStringA("WM_SIZE\n");
-		} break;
-		case WM_DESTROY:
-		{
-			OutputDebugStringA("WM_DESTROY\n");
-		} break;
-		case WM_CLOSE:
-		{
-			OutputDebugStringA("WM_CLOSE\n");
-		} break;
-		case WM_ACTIVATEAPP:
-		{
-			OutputDebugStringA("WM_ACTIVATEAPP\n");
-		} break;
-		case WM_PAINT:
-		{
-			PAINTSTRUCT Paint;
-			HDC DeviceContext = BeginPaint(Window, &Paint);
-			int X = Paint.rcPaint.left;
-			int Y = Paint.rcPaint.top;
-			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-			PatBlt(DeviceContext, X, Y, Width, Height, BLACKNESS);
-			EndPaint(Window, &Paint);
-		} break;
-		default:
-		{
-			//OutputDebugStringA("default\n");
-			result = DefWindowProc(Window, Message, wParam, lParam);
-		} break;
-	}
-	return result;
+LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM wParam,
+                                    LPARAM lParam) {
+  LRESULT result;
+  switch (Message) {
+  case WM_SIZE: {
+    OutputDebugStringA("WM_SIZE\n");
+  } break;
+  case WM_DESTROY: {
+    OutputDebugStringA("WM_DESTROY\n");
+  } break;
+  case WM_CLOSE: {
+    OutputDebugStringA("WM_CLOSE\n");
+  } break;
+  case WM_ACTIVATEAPP: {
+    OutputDebugStringA("WM_ACTIVATEAPP\n");
+  } break;
+  case WM_PAINT: {
+    PAINTSTRUCT Paint;
+    HDC DeviceContext = BeginPaint(Window, &Paint);
+    int X = Paint.rcPaint.left;
+    int Y = Paint.rcPaint.top;
+    int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+    int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+    PatBlt(DeviceContext, X, Y, Width, Height, BLACKNESS);
+    EndPaint(Window, &Paint);
+  } break;
+  default: {
+    // OutputDebugStringA("default\n");
+    result = DefWindowProc(Window, Message, wParam, lParam);
+  } break;
+  }
+  return result;
 }
 
+int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance,
+                     LPSTR CommandLine, int ShowCode) {
+  WNDCLASS WindowClass = {0};
+  WindowClass.style = CS_OWNDC;
+  WindowClass.lpfnWndProc = MainWindowCallback;
+  WindowClass.hInstance = Instance;
+  WindowClass.lpszClassName = "HandmadeHeroWindowClass";
 
-int CALLBACK 
-WinMain(
-    HINSTANCE Instance,
-    HINSTANCE PrevInstance,
-    LPSTR CommandLine,
-    int ShowCode)
-{
-	WNDCLASS WindowClass = {0};
-	WindowClass.style = CS_OWNDC;
-	WindowClass.lpfnWndProc = MainWindowCallback;
-	WindowClass.hInstance = Instance;
-	WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+  if (RegisterClassA(&WindowClass)) {
+    HWND WindowHandle = CreateWindowExA(
+        0, WindowClass.lpszClassName, "Handmade Hero",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, Instance, 0);
 
-	if (RegisterClassA(&WindowClass))
-	{
-		HWND WindowHandle = CreateWindowExA(
-			0,
-			WindowClass.lpszClassName,
-			"Handmade Hero",
-			WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-			CW_USEDEFAULT,
-			CW_USEDEFAULT,
-			CW_USEDEFAULT,
-			CW_USEDEFAULT,
-			0,
-			0,
-			Instance,
-			0
-			);
+    if (WindowHandle) {
+      MSG Message;
+      for (;;) {
+        BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
+        if (MessageResult > 0) {
+          TranslateMessage(&Message);
+          DispatchMessageA(&Message);
+        } else {
+          break;
+        }
+      }
+    } else {
+    }
+  }
 
-		if (WindowHandle)
-		{
-			MSG Message;
-			for (;;)
-			{
-				BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
-				if (MessageResult > 0)
-				{
-					TranslateMessage(&Message);
-					DispatchMessageA(&Message);
-				}
-				else
-				{
-					break;
-				}
-			}
-		}
-		else
-		{
-
-		}
-	}
-
-
-    return 0;
+  return 0;
 }
